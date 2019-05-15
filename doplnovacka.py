a2a62a74d2d69e5fe31297aa76dade3d1063013f
Lucie Hoferkova <luciehoferkova@icloud.com>
2019-05-15 21:13:55 +0200

    Optimalizovane na funkci

--- doplnovacka.py
+++ doplnovacka.py
@@ -37,62 +37,65 @@ původní slovo přidat do seznamu
 seznam spojit pomocí mezery do řetězce, řetězec uložit do nového souboru
 """
 
-with open("song.txt") as f:
-	text = f.read()
-f.closed
 
-text = text.replace("\n", "# ")
+def nahrazeni_slova(soubor_nebo_text, kolikate_slovo_nahradit):
+	
+	try:
+		# Otevreni souboru, nacteni obsahu do promene text
+		f = open(soubor_nebo_text, 'r')
+		text = f.read()
+		
+		# Zavreni souboru (f.closed je spatne -- vraci jestli je soubor otevreny nebo ne)
+		f.close()
 
-# print(text)
-seznam = []
-seznam = text.split(" ")
-# print(seznam)
-vysledek = []
-interpunkce = ".,?!-;\""
+	except FileNotFoundError: 
+		# Kdyz se nejedna o soubor, beru vstuni parametr jako text
+		text = soubor_nebo_text
 
-for i in range(0, len(seznam)):
-	slovo = seznam[i]
-	slovoUpraveno = ""
-	if ((i + 1) % 5 == 0):
-		for pismeno in slovo:
-			if pismeno not in interpunkce:
-				slovoUpraveno += "*"
-			else:
-				slovoUpraveno += pismeno
-		vysledek.append(slovoUpraveno)
-	else:
-		vysledek.append(slovo)
+	# Nahrazeni novych radku za mrizku s mezerou
+	text = text.replace("\n", "# ")
 
-s = " ".join(vysledek)
-s = s.replace("#", "\n")
-print(s)
+	# print(text)
+	
+	# Rozdeleni textu na jednotlive casti podle oddepovace "mezera"
+	seznam = text.split(" ")
 
+	# print(seznam)
 
+	vysledek = []
+	interpunkce = ".,?!-;\""
 
-# with open("song.txt") as f:
-# 	text = f.read()
-# f.closed
+	for i in range(0, len(seznam)):
+		slovo = seznam[i]
+		slovoUpraveno = ""
 
-# text = text.replace("\n", "# ")
+		if ((i + 1) % kolikate_slovo_nahradit == 0):
+			# Pokud je poradi aktualniho slova rovno nasobku parametru "kolikate_slovo_nahradit"
 
-# # print(text)
-# seznam = []
-# seznam = text.split(" ")
-# # print(seznam)
-# vysledek = []
+			# Projdi vsechny pismena v danem slove
+			for znak in slovo:
 
-# for i in range(0, len(seznam)):
-# 	slovo = seznam[i]
-# 	slovoUpraveno = ""
-# 	if ((i + 1) % 5 == 0):
-# 		for pismeno in slovo:
-# 			slovoUpraveno += "-"
-# 		vysledek.append(slovoUpraveno)
-# 	else:
-# 		vysledek.append(slovo)
+				if znak not in interpunkce:
+					# Pokud znak neni interpunkce, nahrad hvezdickou
 
-# s = " ".join(vysledek)
-# print(s)
+					slovoUpraveno += "*"
+				else:
+					# Jinak ponech nezmeneny znak
+					slovoUpraveno += znak
+
+			vysledek.append(slovoUpraveno)
+		else:
+			# Poradi slova neni nasobkem "kolikate_slovo_nahradit"
+			vysledek.append(slovo)
+
+	s = " ".join(vysledek)
+	s = s.replace("#", "\n")
+
+	# Vytiskni vysledek
+	print(s)
 
 
+soubor = input("Zadej nazev souboru, ktery se ma nacist nebo text: ")
+cislo = int(input("Zadej cele cislo - kolikate slovo textu se ma zamenit: "))
 
+nahrazeni_slova(soubor, kolikate_slovo_nahradit=cislo)
